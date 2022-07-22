38df05fd07248c399c1429fa7f020e73076604a1
mfaiquzair <83383135+mfaiquzair@users.noreply.github.com>
2022-07-22 22:01:33 +0800

    lab 8 WE class
    php

---
+++ q4lab8.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>The result is : </p>
+<?php 
+$str = "do you think PHP is a good scripting language?";
+echo strlen($str);
+echo "<br/>";
+echo strpos($str, "PHP");
+?>
+</body>
+</html>
